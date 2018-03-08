5b35b6d4c0542e05bfaa1c66962942c740e0cae2
Jorge Santos <jorge_miguel@outlook.com>
2018-03-08 02:21:34 +0000

    Add TestProject

---
+++ TDDMicroExercises.UTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TDDMicroExercises.UTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TDDMicroExercises.UTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("21c4df12-1bd8-44c9-96db-13b3b6dedda3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TDDMicroExercises.UTest/TirePressureMonitoringSystem/AlarmTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDDMicroExercises.UTest
+{
+    [TestClass]
+    public class AlarmTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
